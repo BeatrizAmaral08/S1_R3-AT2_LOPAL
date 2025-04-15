ce5614671419c5ffc8d0c773b93ef420f7c81a28
unknown <beatriz.sandron@aluno.senai.br>
2025-04-15 09:27:08 -0300

    Organizar commits

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // pede para o usuario digitar um numero aleatoio
 let numero = prompt("Digite um número:");
 
-// le o valor digitado 
-numero = Number(numero);
-
 // confere se o numero é maior ou menor que 0
 if (numero > 0) {
   alert("Número é positivo!");

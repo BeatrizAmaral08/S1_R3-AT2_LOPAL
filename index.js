b059b97290a6ca98c1ae44c6faee550ae4bcb2d5
unknown <beatriz.sandron@aluno.senai.br>
2025-04-15 08:37:58 -0300

    crie um algoritmo que defina se um numero é maior ou menor que 0, dizendo se é positivo ou negativo

---
+++ index.js
@@ -0,0 +1,12 @@
+// pede para o usuario digitar um numero aleatoio
+let numero = prompt("Digite um número:");
+
+// le o valor digitado 
+numero = Number(numero);
+
+// confere se o numero é maior ou menor que 0
+if (numero > 0) {
+  alert("Número é positivo!");
+} else {
+  alert("Número não é positivo!");
+}
